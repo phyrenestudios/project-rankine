73893f67a9eb0f1a827e3f69e7637d72536bd020
Patrick <patn8092@gmail.com>
2020-05-14 11:57:59 -0700

    Added slab and stair varieties for smooth stone types. Refractory bricks and magnesium refractory bricks as well. Double Slab textures for smooth stones.

---
+++ src/main/java/com/cannolicatfish/rankine/blocks/RankineSlab.java
@@ -0,0 +1,11 @@
+package com.cannolicatfish.rankine.blocks;
+
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class RankineSlab extends SlabBlock {
+    public RankineSlab(Properties properties) {
+        super(properties);
+    }
+}
