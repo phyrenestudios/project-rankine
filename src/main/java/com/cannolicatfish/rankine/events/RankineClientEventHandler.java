b84ed9febd678b339d8c9beb3f660b1ed748f61d
Hunter19823 <hspragg@asu.edu>
2022-03-24 15:54:25 -0700

    Optimized Imports  - Removed unused imports and star imports.

--- src/main/java/com/cannolicatfish/rankine/events/RankineClientEventHandler.java
+++ src/main/java/com/cannolicatfish/rankine/events/RankineClientEventHandler.java
@@ -1,32 +1,18 @@
 package com.cannolicatfish.rankine.events;
 
-import com.cannolicatfish.rankine.blocks.GasBlock;
+
 import com.cannolicatfish.rankine.events.handlers.client.FOVUpdateHandler;
 import com.cannolicatfish.rankine.events.handlers.client.RecipesUpdateHandler;
 import com.cannolicatfish.rankine.events.handlers.client.RenderBlockOverlayHandler;
 import com.cannolicatfish.rankine.events.handlers.client.RenderOverlayHandler;
-import com.cannolicatfish.rankine.init.*;
-import com.cannolicatfish.rankine.items.tools.KnifeItem;
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.WorldVertexBufferUploader;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Matrix4f;
-import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.*;
+import net.minecraftforge.client.event.FOVUpdateEvent;
+import net.minecraftforge.client.event.RecipesUpdatedEvent;
+import net.minecraftforge.client.event.RenderBlockOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.lwjgl.opengl.GL11;
 
 @Mod.EventBusSubscriber
 public class RankineClientEventHandler {
