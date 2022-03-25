39ddc7c8c025f4f5a38cd0e17e4f755cfa533554
Hunter19823 <hspragg@asu.edu>
2022-03-24 17:52:55 -0700

    Cleaned up RenderOverlay to look cleaner. Stored static texture location rather than recreating it multiple times. Extracted Duplicate code into its own method.

--- src/main/java/com/cannolicatfish/rankine/events/handlers/client/RenderOverlayHandler.java
+++ src/main/java/com/cannolicatfish/rankine/events/handlers/client/RenderOverlayHandler.java
@@ -20,43 +20,23 @@ import org.lwjgl.opengl.GL11;
 
 public class RenderOverlayHandler {
 
-    public static void renderOverlay( RenderGameOverlayEvent event) {
-        if (event.getType() == RenderGameOverlayEvent.ElementType.HELMET && Minecraft.getInstance().player != null) {
-            PlayerEntity player = Minecraft.getInstance().player;
-            ItemStack stack = player.getHeldItemOffhand().getItem() instanceof KnifeItem ? player.getHeldItemOffhand() : ItemStack.EMPTY;
-            if (!stack.isEmpty()) {
-                int i = stack.getItem().getUseDuration(stack) - player.getItemInUseCount();
-                if (i < (10 + EnchantmentHelper.getEnchantmentLevel(RankineEnchantments.PREPARATION, stack))) {
-                    GL11.glPushMatrix();
-                    GL11.glEnable(GL11.GL_BLEND);
-                    RenderSystem.disableDepthTest();
-                    RenderSystem.depthMask(false);
-                    RenderSystem.defaultBlendFunc();
-                    RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-                    RenderSystem.disableAlphaTest();
-                    Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("rankine:textures/misc/parry_overlay.png"));
-                    Tessellator tessellator = Tessellator.getInstance();
-                    BufferBuilder bufferbuilder = tessellator.getBuffer();
-                    bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-                    bufferbuilder.pos(0.0D, (double)event.getWindow().getScaledHeight(), -90.0D).tex(0.0F, 1.0F).endVertex();
-                    bufferbuilder.pos((double)event.getWindow().getScaledWidth(), (double)event.getWindow().getScaledHeight(), -90.0D).tex(1.0F, 1.0F).endVertex();
-                    bufferbuilder.pos((double)event.getWindow().getScaledWidth(), 0.0D, -90.0D).tex(1.0F, 0.0F).endVertex();
-                    bufferbuilder.pos(0.0D, 0.0D, -90.0D).tex(0.0F, 0.0F).endVertex();
-                    tessellator.draw();
-                    RenderSystem.depthMask(true);
-                    RenderSystem.enableDepthTest();
-                    RenderSystem.enableAlphaTest();
-                    RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-                    GL11.glPopMatrix();
-                }
-            }
-        }
-        if (event.getType() == RenderGameOverlayEvent.ElementType.HELMET && Minecraft.getInstance().player != null) {
-            PlayerEntity player = Minecraft.getInstance().player;
-            World worldIn = player.getEntityWorld();
+    private static final ResourceLocation TEXTURE = new ResourceLocation("rankine:textures/misc/parry_overlay.png");
 
-            Block bl = worldIn.getBlockState(new BlockPos(player.getPosX(), player.getPosYEye(), player.getPosZ())).getBlock();
-            if (bl instanceof GasBlock) {
+    public static void renderOverlay( RenderGameOverlayEvent event) {
+        // Return if the player is null.
+        if(Minecraft.getInstance().player == null)
+            return;
+        // Return if the render is not for the helmet.
+        if(event.getType() != RenderGameOverlayEvent.ElementType.HELMET)
+            return;
+
+        PlayerEntity player = Minecraft.getInstance().player;
+        ItemStack stack = player.getHeldItemOffhand().getItem() instanceof KnifeItem ? player.getHeldItemOffhand() : ItemStack.EMPTY;
+        World worldIn = player.getEntityWorld();
+
+        if (!stack.isEmpty()) {
+            int i = stack.getItem().getUseDuration(stack) - player.getItemInUseCount();
+            if (i < (10 + EnchantmentHelper.getEnchantmentLevel(RankineEnchantments.PREPARATION, stack))) {
                 GL11.glPushMatrix();
                 GL11.glEnable(GL11.GL_BLEND);
                 RenderSystem.disableDepthTest();
@@ -64,22 +44,39 @@ public class RenderOverlayHandler {
                 RenderSystem.defaultBlendFunc();
                 RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
                 RenderSystem.disableAlphaTest();
-                Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("rankine:textures/block/"+bl.getRegistryName().getPath()+".png"));
-                Tessellator tessellator = Tessellator.getInstance();
-                BufferBuilder bufferbuilder = tessellator.getBuffer();
-                bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-                bufferbuilder.pos(0.0D, (double)event.getWindow().getScaledHeight(), -90.0D).tex(0.0F, 1.0F).endVertex();
-                bufferbuilder.pos((double)event.getWindow().getScaledWidth(), (double)event.getWindow().getScaledHeight(), -90.0D).tex(1.0F, 1.0F).endVertex();
-                bufferbuilder.pos((double)event.getWindow().getScaledWidth(), 0.0D, -90.0D).tex(1.0F, 0.0F).endVertex();
-                bufferbuilder.pos(0.0D, 0.0D, -90.0D).tex(0.0F, 0.0F).endVertex();
-                tessellator.draw();
-                RenderSystem.depthMask(true);
-                RenderSystem.enableDepthTest();
-                RenderSystem.enableAlphaTest();
-                RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-                GL11.glPopMatrix();
+                Minecraft.getInstance().getTextureManager().bindTexture(TEXTURE);
+                tessellateLogic(event);
             }
         }
+
+        Block bl = worldIn.getBlockState(new BlockPos(player.getPosX(), player.getPosYEye(), player.getPosZ())).getBlock();
+        if (bl instanceof GasBlock) {
+            GL11.glPushMatrix();
+            GL11.glEnable(GL11.GL_BLEND);
+            RenderSystem.disableDepthTest();
+            RenderSystem.depthMask(false);
+            RenderSystem.defaultBlendFunc();
+            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+            RenderSystem.disableAlphaTest();
+            Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("rankine:textures/block/"+bl.getRegistryName().getPath()+".png"));
+            tessellateLogic(event);
+        }
+    }
+
+    private static void tessellateLogic( RenderGameOverlayEvent event ) {
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos(0.0D, event.getWindow().getScaledHeight(), -90.0D).tex(0.0F, 1.0F).endVertex();
+        bufferbuilder.pos(event.getWindow().getScaledWidth(), event.getWindow().getScaledHeight(), -90.0D).tex(1.0F, 1.0F).endVertex();
+        bufferbuilder.pos(event.getWindow().getScaledWidth(), 0.0D, -90.0D).tex(1.0F, 0.0F).endVertex();
+        bufferbuilder.pos(0.0D, 0.0D, -90.0D).tex(0.0F, 0.0F).endVertex();
+        tessellator.draw();
+        RenderSystem.depthMask(true);
+        RenderSystem.enableDepthTest();
+        RenderSystem.enableAlphaTest();
+        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+        GL11.glPopMatrix();
     }
 
 }
